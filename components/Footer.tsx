8f8aaadfe17451d04247d00cc4a2f508b90ec230
AliDev2205 <alimilamidi24@gmail.com>
2026-03-05 11:44:12 +0100

    UI: Update footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -133,7 +133,7 @@ export default function Footer() {
                         </div>
                         <div className="mt-4 md:mt-0 flex items-center gap-2 text-sm text-slate-400">
                             <i className="fa-solid fa-heart text-rose-500 animate-pulse"></i>
-                            Made with love for better smiles by{" "}
+                            Réalisé par{" "}
                             <a
                                 href="https://wa.me/22965302251"
                                 target="_blank"
